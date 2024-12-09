b029dc710ad476d534ebb22925d4b20ee6954fcd
Taha Saif <160765440+Tahasaif3@users.noreply.github.com>
2024-12-10 03:22:36 +0500

    Update stats.tsx

--- components/stats.tsx
+++ components/stats.tsx
@@ -2,25 +2,25 @@ import Image from 'next/image'
 
 export default function StatisticsSection() {
   return (
-    <section className="relative py-16">
+    <section className="relative py-6 mt-0">
       <Image
         src="/stats.png?height=468&width=1700&text=Background+Image"
         alt="Background"
-        layout="fill"
-        objectFit="cover"
-        className="absolute inset-0 w-full h-full brightness-50" />
-        
+        width={1700}
+       height={100}
+        className="absolute inset-0 w-full h-full brightness-50"
+      />
       <div className="relative z-10 container mx-auto px-4">
         <div className="flex flex-wrap justify-between items-center">
-       <Image
-        src={"/st.png"}
-        alt='icon'
-        width={1319}
-        height={247}
-       />
+          <Image
+            src="/st.png"
+            alt="Icon"
+            width={1319}
+            height={247}
+            className="w-full object-contain"
+          />
         </div>
       </div>
     </section>
-  )
+  );
 }
-
