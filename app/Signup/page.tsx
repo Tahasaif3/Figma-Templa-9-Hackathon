47c08a8e1667629a709d4b2d035cd8f30ca4ab48
Taha Saif <160765440+Tahasaif3@users.noreply.github.com>
2024-12-10 03:48:59 +0500

    Update page.tsx

--- app/Signup/page.tsx
+++ app/Signup/page.tsx
@@ -143,9 +143,11 @@ export default function SignUpPage() {
                 <FaApple width={20} height={20} className="mr-2" />
                  Sign up with Apple
               </button>
-             <Link href={"/Signin"}><button className="w-[370px] h-[50px] flex justify-center items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-100 hover:border-2 hover:border-orange-400 ">
-                 Already Have an Account Login in?
-              </button></Link>
+                <Link href={"/Signin"}>
+                <button className="w-full sm:w-auto h-[50px] flex justify-center items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-center text-sm font-medium text-gray-700 bg-white hover:bg-gray-100 hover:border-2 hover:border-orange-400">
+                  Already Have an Account? Log in
+                </button>
+              </Link>
             </div>
           </div>
         </div>
