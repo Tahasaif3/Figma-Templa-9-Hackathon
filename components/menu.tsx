c9258b46347aceda63f3dc5fc9454eacc1290ba6
Taha Saif <160765440+Tahasaif3@users.noreply.github.com>
2024-12-11 01:51:10 +0500

    menu.tsx

--- components/menu.tsx
+++ components/menu.tsx
@@ -32,7 +32,7 @@ export default function MenuSection() {
           />
         </div>
         <h3 className="text-4xl font-bold mb-8 flex justify-center items-center ml-8">From Our Menu</h3>
- 
+
         {/* Menu Categories */}
         <div className="flex justify-center items-center mb-10 sm:mb-4">
           <Image
@@ -83,16 +83,17 @@ export default function MenuSection() {
         </div>
 
         {/* Chef Section */}
-        <div className="container mx-auto px-4 ">
+        <div className="container mx-auto px-4">
           <div className="flex justify-center">
             <Image
               src={"/Chefs.png"}
               width={59}
               height={40}
               alt='blogs logo'
+              className='mt-6'
             />
-          </div>        <h3 className="text-4xl font-bold mb-8 flex justify-center items-center"><span className='text-orange-500'>Me</span>et Our Chef</h3>
-
+          </div>       
+           <h3 className="text-4xl font-bold mb-8 flex justify-center items-center"><span className='text-orange-500'>Me</span>et Our Chef</h3>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
             {chefs.map((chef, index) => (
               <div key={index} className="text-center">
